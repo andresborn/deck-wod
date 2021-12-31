4235c2689402db5e63d8b51656dfaf3a7497eb66
andresborn <andres.born@gmail.com>
2021-12-31 12:57:49 -0600

    init

---
+++ src/Card.svelte
@@ -0,0 +1,171 @@
+<!-- Card.svelte -->
+<script lang="ts">
+	type Card = {
+		number: string;
+		suit: string;
+	};
+	export let deckLength: number;
+	export let cardValue: Card;
+	export let handleClick;
+	export let currentExcercise: string;
+	export let handleReset;
+
+	const suitDic = {
+		Espada: '♠',
+		Diamante: '♦',
+		Corazón: '♥',
+		Trébol: '♣',
+		Joker: '🤡'
+	};
+
+	const getCardNumber = (number: string) => {
+		if (number === 'Q' || number === 'K' || number === 'J') {
+			return '10';
+		}
+		if (number === 'A') {
+			return '11';
+		} else return number;
+	};
+</script>
+
+{#if cardValue !== undefined}
+	<div class="container">
+		<p>Do {getCardNumber(cardValue.number)} {currentExcercise}!</p>
+
+		<div class="card">
+			<div class="top">
+				<div class="icon">
+					<div>
+						{suitDic[cardValue.suit]}
+					</div>
+					<div>
+						{cardValue.number === '15' ? '' : cardValue.number}
+					</div>
+				</div>
+			</div>
+			<div class="mid">
+				<div class="icon">
+					{suitDic[cardValue.suit]}
+				</div>
+			</div>
+			<div class="bottom">
+				<div class="icon">
+					<div>
+						{suitDic[cardValue.suit]}
+					</div>
+					<div>
+						{cardValue.number === '15' ? '' : cardValue.number}
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<button class="button" on:click={handleClick}>Change Card</button>
+	</div>
+{:else if deckLength === 0}
+	<div class="container">
+		<p>Let's workout again!</p>
+		<div class="initialCard" />
+		<button class="button" on:click={handleReset}>Start Over</button>
+	</div>
+{:else}
+	<div class="container">
+		<p>Let's workout!</p>
+		<div class="initialCard" />
+		<button class="button" on:click={handleClick}>Change Card</button>
+	</div>
+{/if}
+
+<style>
+	p {
+		font-size: 1.5rem;
+	}
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.card {
+		width: 15rem;
+		height: 25rem;
+		background-color: aliceblue;
+		display: flex;
+		flex-direction: column;
+		box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+		border-radius: 10px;
+		margin-bottom: 2rem;
+	}
+	.initialCard {
+		width: 15rem;
+		height: 25rem;
+		background-color: rgb(129, 196, 255);
+		display: flex;
+		flex-direction: column;
+		box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+		border-radius: 10px;
+		margin-bottom: 2rem;
+	}
+	.top {
+		display: flex;
+		justify-content: flex-start;
+		margin: auto 0;
+		margin-top: 0;
+		margin-left: 0.2rem;
+	}
+	.mid {
+		display: flex;
+		justify-content: center;
+		margin: auto 0;
+	}
+	.bottom {
+		display: flex;
+		justify-content: flex-end;
+		margin: auto 0;
+		margin-bottom: 0;
+		margin-right: 0.2rem;
+	}
+	.icon {
+		display: flex;
+		flex-direction: column;
+		margin: 1rem;
+		font-size: 2rem;
+	}
+	.button {
+		background-color: #111827;
+		border: 1px solid transparent;
+		border-radius: 0.75rem;
+		box-sizing: border-box;
+		color: #ffffff;
+		cursor: pointer;
+		flex: 0 0 auto;
+		font-family: 'Inter var', ui-sans-serif, system-ui, -apple-system, system-ui, 'Segoe UI', Roboto,
+			'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+			'Segoe UI Symbol', 'Noto Color Emoji';
+		font-size: 1.125rem;
+		font-weight: 600;
+		line-height: 1.5rem;
+		padding: 0.75rem 1.2rem;
+		text-align: center;
+		text-decoration: none #6b7280 solid;
+		text-decoration-thickness: auto;
+		transition-duration: 0.2s;
+		transition-property: background-color, border-color, color, fill, stroke;
+		transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+		user-select: none;
+		-webkit-user-select: none;
+		touch-action: manipulation;
+		width: auto;
+	}
+
+	.button:hover {
+		background-color: #374151;
+	}
+
+	.button:focus {
+		box-shadow: none;
+		outline: 2px solid transparent;
+		outline-offset: 2px;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,108 @@
+<script lang="ts">
+	import Card from '../Card.svelte';
+	const deck = [
+		{ suit: 'Trébol', number: 'A' },
+		{ suit: 'Trébol', number: '2' },
+		{ suit: 'Trébol', number: '3' },
+		{ suit: 'Trébol', number: '4' },
+		{ suit: 'Trébol', number: '5' },
+		{ suit: 'Trébol', number: '6' },
+		{ suit: 'Trébol', number: '7' },
+		{ suit: 'Trébol', number: '8' },
+		{ suit: 'Trébol', number: '9' },
+		{ suit: 'Trébol', number: '10' },
+		{ suit: 'Trébol', number: 'J' },
+		{ suit: 'Trébol', number: 'Q' },
+		{ suit: 'Trébol', number: 'K' },
+		{ suit: 'Corazón', number: 'A' },
+		{ suit: 'Corazón', number: '2' },
+		{ suit: 'Corazón', number: '3' },
+		{ suit: 'Corazón', number: '4' },
+		{ suit: 'Corazón', number: '5' },
+		{ suit: 'Corazón', number: '6' },
+		{ suit: 'Corazón', number: '7' },
+		{ suit: 'Corazón', number: '8' },
+		{ suit: 'Corazón', number: '9' },
+		{ suit: 'Corazón', number: '10' },
+		{ suit: 'Corazón', number: 'J' },
+		{ suit: 'Corazón', number: 'Q' },
+		{ suit: 'Corazón', number: 'K' },
+		{ suit: 'Espada', number: 'A' },
+		{ suit: 'Espada', number: '2' },
+		{ suit: 'Espada', number: '3' },
+		{ suit: 'Espada', number: '4' },
+		{ suit: 'Espada', number: '5' },
+		{ suit: 'Espada', number: '6' },
+		{ suit: 'Espada', number: '7' },
+		{ suit: 'Espada', number: '8' },
+		{ suit: 'Espada', number: '9' },
+		{ suit: 'Espada', number: '10' },
+		{ suit: 'Espada', number: 'J' },
+		{ suit: 'Espada', number: 'Q' },
+		{ suit: 'Espada', number: 'K' },
+		{ suit: 'Diamante', number: 'A' },
+		{ suit: 'Diamante', number: '2' },
+		{ suit: 'Diamante', number: '3' },
+		{ suit: 'Diamante', number: '4' },
+		{ suit: 'Diamante', number: '5' },
+		{ suit: 'Diamante', number: '6' },
+		{ suit: 'Diamante', number: '7' },
+		{ suit: 'Diamante', number: '8' },
+		{ suit: 'Diamante', number: '9' },
+		{ suit: 'Diamante', number: '10' },
+		{ suit: 'Diamante', number: 'J' },
+		{ suit: 'Diamante', number: 'Q' },
+		{ suit: 'Diamante', number: 'K' },
+		{ suit: 'Joker', number: '15' },
+		{ suit: 'Joker', number: '15' }
+	];
+	const excercises = [
+		{ suit: 'Trébol', excercise: 'Squats' },
+		{ suit: 'Corazón', excercise: 'Push Ups' },
+		{ suit: 'Diamante', excercise: 'Abs' },
+		{ suit: 'Espada', excercise: 'Lunges' },
+		{ suit: 'Joker', excercise: 'Burpees' }
+	];
+	type Card = {
+		number: string;
+		suit: string;
+	};
+	let currentCard: Card;
+	let currentExcercise: string;
+
+	let deckOnInit = deck;
+	const getCard = () => {
+		const card = deckOnInit[Math.floor(Math.random() * deckOnInit.length)];
+
+		const cardIndex = deckOnInit.findIndex((c) => c.suit === card.suit && c.number === card.number);
+		// Remove card from deck
+		deckOnInit = deckOnInit.filter((c, idx) => idx !== cardIndex);
+		// Set states
+		currentCard = card;
+		currentExcercise = excercises.find((e) => e.suit === card.suit).excercise;
+	};
+
+	const reset = () => {
+		deckOnInit = deck;
+	};
+</script>
+
+<div class="container">
+	<h1>Deck of Cards WOD</h1>
+	<Card
+		cardValue={currentCard}
+		deckLength={deckOnInit.length}
+		{currentExcercise}
+		handleClick={getCard}
+		handleReset={reset}
+	/>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		background-color: #fff;
+	}
+</style>
